82561edf34cb1ae25b3b493e8a8254be0104dc41
Hirisci <www.alan92@icloud.com>
2022-05-23 20:50:41 +0200

    implementato add

--- js/main.js
+++ js/main.js
@@ -2,27 +2,21 @@ var app = new Vue({
     el: '#app',
     data: {
       date: new Date(),
+      isTaskDoneVisible: true,
+      openMenu: false,
       todos: [
           {
-            msg: "fare la spesa", place: "milano", date : "05/05/22", isDone: false, 
+            msg: "fare la spesa", place: "milano", date : "05/05/22", isDone: false, type: true
           },
           {
-            msg: "fare la spesa 2", place: "milano", date : "05/05/22", isDone: false, 
+            msg: "fare la spesa 2", place: "milano", date : "05/05/22", isDone: false, type: true
           },
-          {
-            msg: "fare la spesa 3", place: "milano", date : "05/05/22", isDone: false, 
-          },
-          {
-            msg: "fare la spesa 4", place: "milano", date : "05/05/22", isDone: false, 
-          },
-          {
-            msg: "fare la spesa 5", place: "milano", date : "05/05/22", isDone: false, 
-          },
-          {
-            msg: "fare la spesa 6", place: "milano", date : "05/05/22", isDone: false, 
-          },
+     
           
-        ]
+        ],
+        title: '',
+        where: '',
+        selected: "",
     },
     methods: {
         completeToDo(todo){
@@ -33,23 +27,44 @@ var app = new Vue({
         },
         removeToDo(i){
             this.todos[i].isDone ? this.todos.splice(i-1,1) : this.todos.splice(i,1)
+        },
+        addToDo(){
+            let ogg = {msg: this.title, place: this.where, date : "05/05/22", isDone: false, type: true}
+            this.selected==="personal" ? ogg.type = true : ogg.type = false;
+            console.log(this.selected);
+            this.todos.push(ogg)
+            this.menuOpen();
+        },
+        toggleUl(){
+            this.isTaskDoneVisible = !this.isTaskDoneVisible;
+        },
+        menuOpen(){
+            this.openMenu= !this.openMenu
         }
         
 
     },
+
     computed: {
+        todosPersonal(){
+            return this.todos.filter((task)=>task.type)
+        },
+        todosWork(){
+            return this.todos.filter((task)=>!task.type)
+        },
         todosComplete(){
             return this.todos.filter((task)=>task.isDone)
         },
-        todosUncomlete(){
+        todosUncomplete(){
             return this.todos.filter((task)=>!task.isDone)
-        },
+        }, 
         printDate(){
             var day=this.date.getDate();  
             var month=this.date.getMonth()+1;  
             var year=this.date.getFullYear();  
             return `${day}/${month}/${year}`
-        }
+        },
+      
     }
   })
 
