60e099ed098a4e8e2161312feb0957a3cd4e36c7
Hirisci <www.alan92@icloud.com>
2022-05-23 17:06:41 +0200

    implementata logica primitiva

--- js/main.js
+++ js/main.js
@@ -1,12 +1,49 @@
 var app = new Vue({
     el: '#app',
     data: {
-      
+      todos: [
+          {
+            msg: "fare la spesa", place: "milano", date : "05/05/22", isDone: false, 
+          },
+          {
+            msg: "fare la spesa", place: "milano", date : "05/05/22", isDone: false, 
+          },
+          {
+            msg: "fare la spesa", place: "milano", date : "05/05/22", isDone: false, 
+          },
+          {
+            msg: "fare la spesa", place: "milano", date : "05/05/22", isDone: false, 
+          },
+          {
+            msg: "fare la spesa", place: "milano", date : "05/05/22", isDone: false, 
+          },
+          {
+            msg: "fare la spesa", place: "milano", date : "05/05/22", isDone: false, 
+          },
+          
+        ]
     },
     methods: {
-      
+        completeToDo(todo){
+           todo.isDone=!todo.isDone
+        },
+        editToDo(todo){
+
+        },
+        removeToDo(i){
+            this.todos.splice(i,1)
+            
+        }
+
     },
     computed: {
-      
+        todosComplete(){
+            return this.todos.filter((task)=>task.isDone)
+        },
+        todosUncomlete(){
+            return this.todos.filter((task)=>!task.isDone)
+        }
     }
   })
+
+
