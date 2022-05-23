0ff2ff9c420df462ddb755886504a18e55e60bf8
Hirisci <www.alan92@icloud.com>
2022-05-23 17:40:34 +0200

    aggiunta logica

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
 var app = new Vue({
     el: '#app',
     data: {
+      date: new Date(),
       todos: [
           {
             msg: "fare la spesa", place: "milano", date : "05/05/22", isDone: false, 
           },
           {
-            msg: "fare la spesa", place: "milano", date : "05/05/22", isDone: false, 
+            msg: "fare la spesa 2", place: "milano", date : "05/05/22", isDone: false, 
           },
           {
-            msg: "fare la spesa", place: "milano", date : "05/05/22", isDone: false, 
+            msg: "fare la spesa 3", place: "milano", date : "05/05/22", isDone: false, 
           },
           {
-            msg: "fare la spesa", place: "milano", date : "05/05/22", isDone: false, 
+            msg: "fare la spesa 4", place: "milano", date : "05/05/22", isDone: false, 
           },
           {
-            msg: "fare la spesa", place: "milano", date : "05/05/22", isDone: false, 
+            msg: "fare la spesa 5", place: "milano", date : "05/05/22", isDone: false, 
           },
           {
-            msg: "fare la spesa", place: "milano", date : "05/05/22", isDone: false, 
+            msg: "fare la spesa 6", place: "milano", date : "05/05/22", isDone: false, 
           },
           
         ]
@@ -31,9 +32,9 @@ var app = new Vue({
 
         },
         removeToDo(i){
-            this.todos.splice(i,1)
-            
+            this.todos[i].isDone ? this.todos.splice(i-1,1) : this.todos.splice(i,1)
         }
+        
 
     },
     computed: {
@@ -42,6 +43,12 @@ var app = new Vue({
         },
         todosUncomlete(){
             return this.todos.filter((task)=>!task.isDone)
+        },
+        printDate(){
+            var day=this.date.getDate();  
+            var month=this.date.getMonth()+1;  
+            var year=this.date.getFullYear();  
+            return `${day}/${month}/${year}`
         }
     }
   })
